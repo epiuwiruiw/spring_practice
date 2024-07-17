76c88685b33bdd10b056438db8201a69b6d43355
epiuwiruiw <mandoo2276@naver.com>
2024-07-17 16:16:49 +0900

    spring_practice

---
+++ demo/src/test/java/com/beyond/board/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyond.board.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
